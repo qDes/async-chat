910ea9deae342e1adeca98f2f86c99262b12520f
Andrei K <1____1@bk.ru>
2019-11-27 14:40:26 +0300

    add configparser to chat listener

--- listen_minechat.py
+++ listen_minechat.py
@@ -1,41 +1,44 @@
-import asyncio 
-import argparse
+import asyncio
+import configargparse
+
 from datetime import datetime
-from aiofile import AIOFile, Reader, Writer
+from aiofile import AIOFile
+
 
 def get_current_time() -> str:
     now = datetime.now()
-    return f"[{now.strftime('%x')} {now.strftime('%X')}] " 
+    return f"[{now.strftime('%x')} {now.strftime('%X')}] "
+
 
 async def client(HOST, PORT, HIST_FILE):
     while True:
+        connection_counter = 0
         try:
-            reader, writer = await asyncio.open_connection(
-                    HOST, PORT)
-        except (ConnectionRefusedError, 
-                ConnectionResetError) as err:
-            print('Нет соединения.')
-        print(f'{get_current_time()} Установлено соединение')
-        
-        async with AIOFile(HIST_FILE, 'a+') as afp:    
+            reader, writer = await asyncio.open_connection(HOST, PORT)
+        except (ConnectionRefusedError,
+                ConnectionResetError):
+            print('Нет соединения. Повторная попытка.')
+            connection_counter += 1
+            if connection_counter > 2:
+                print("Выход.")
+                return None
+        print(f'{get_current_time()}Установлено соединение')
+        async with AIOFile(HIST_FILE, 'a+') as afp:
             while True:
-                data = await reader.read(2048)
-                #get rid of empty messages
-                if len(data) > 2:
-                    message = get_current_time() + data.decode()
-                    await afp.write(message)
-                    print(message)
+                data = await reader.readline()
+                message = get_current_time() + data.decode()
+                print(message)
+                await afp.write(message)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Async connection to minechat')
-    parser.add_argument('--host',action='store',help = 'set chat addr',
-            default = 'minechat.dvmn.org')
-    parser.add_argument('--port',action='store',type=int,help='set char port',
-            default=5000)
-    parser.add_argument('--history',action='store',help='set history file',
-            default='minechat.history')
+    parser = configargparse.ArgParser(default_config_files=['.env_listen'])
+    parser.add('--host', required=True,
+               help='chat host address')
+    parser.add('--port', required=True, type=int,
+               help='chat listen port')
+    parser.add('--history', required=True,
+               help='history writing file')
     args = parser.parse_args()
     HOST, PORT, HIST_FILE = args.host, args.port, args.history
-
     asyncio.run(client(HOST, PORT, HIST_FILE))
