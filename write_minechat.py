90b832bd5202653de5d2a6aee4427285459fd4ed
Andrei K <1____1@bk.ru>
2019-11-29 18:59:21 +0300

    fix cli for minechat writer

--- write_minechat.py
+++ write_minechat.py
@@ -1,25 +1,27 @@
-import os
 import asyncio
 import json
 import logging
 
-from dotenv import load_dotenv
-from listen_minechat import get_current_time
+import configargparse
 from aioconsole import ainput
 
 
-async def register(reader, writer):
+async def register(reader, writer, username):
+    message = '\n'
+    writer.write(message.encode())
+    await writer.drain()
     data = await reader.readline()
-    await submit_message(writer)
+    username += '\n'
+    writer.write(username.encode())
+    await writer.drain()
     data = await reader.readline()
-    print(json.loads(data))
+    profile = json.loads(data)
+    print(f"Your nick: {profile['nickname']}, your token: {profile['account_hash']}")
     return None
 
 
-async def authorise(reader, writer):
-    #await submit_message(writer)
-    message = await ainput(">>> ")
-    message += "\n"
+async def authorise(reader, writer, token):
+    message = token + "\n"
     writer.write(message.encode())
     await writer.drain()
     data = await reader.readline()
@@ -28,32 +30,51 @@ async def authorise(reader, writer):
         logging.debug(f"Пользователь {data['nickname']} авторизован.")
         return True
     else:
-        logging.debug('Токен невалидный. Проверьте его или зарегестрируйтесь заново.')
+        logging.debug("Токен невалидный. Проверьте его или зарегестрируйтесь заново.")
         return False
 
-async def submit_message(writer):
-    message = await ainput(">>> ")
+
+async def submit_message(writer, message=None):
+    if not message:
+        message = await ainput(">>> ")
     message += "\n\n"
+    logging.debug(f"send message - {message}")
     writer.write(message.encode())
     await writer.drain()
 
 
-async def main(host, port):
+async def main(host, port, token, message, username):
+    auth_status = False
     reader, writer = await asyncio.open_connection(host, port)
-    data = await reader.read(2048)
+    data = await reader.readline()
     logging.debug(f"{data.decode()}")
-    authorise_status = await authorise(reader, writer)
-    if not authorise_status:
-        await register(reader, writer)
-    while True:
-        await submit_message(writer)
-
-
+    if token:
+        auth_status = await authorise(reader, writer, token)
+    if not auth_status and not username:
+        logging.debug(f"Enter username for new user register or valid token.")
+        return None
+    if username and not auth_status:
+        await register(reader, writer, username)
+    if message:
+        await submit_message(writer, message)
+    else:
+        while True:
+            await submit_message(writer)
 
 
 if __name__ == "__main__":
     FORMAT = "%(levelname)s:sender: %(message)s"
     logging.basicConfig(format=FORMAT, level=logging.DEBUG)
-    load_dotenv()
-    token = os.environ.get("CHAT_TOKEN")
-    asyncio.run(main('minechat.dvmn.org',5050))
+    parser = configargparse.ArgParser(description='minechat message sender',
+                                      default_config_files=['.env_write'])
+    parser.add("--host", required=True,
+               help="chat host address")
+    parser.add('--port', required=True, type=int,
+               help='chat write port')
+    parser.add('--token', help='user connection token')
+    parser.add('--message', help='message to chat')
+    parser.add('--username', help='username of unregistered user')
+    args = parser.parse_args()
+    host, port, token = args.host, args.port, args.token
+    message, username = args.message, args.username
+    asyncio.run(main(host, port, token, message, username))
