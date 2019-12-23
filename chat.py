9ced8c74eb565161ff652e543a39b6e9c07c97eb
Andrei K <1____1@bk.ru>
2019-12-23 16:47:25 +0300

    add auth via token async function

--- chat.py
+++ chat.py
@@ -2,6 +2,8 @@ import asyncio
 import gui
 import time
 import configargparse
+import logging
+import json
 
 from aiofile import AIOFile
 from datetime import datetime
@@ -48,26 +50,52 @@ async def read_msgs(host, port, queue, history_queue):
                 queue.put_nowait("Выход.")
                 return None
 
-async def send_msgs(host, port, queue):
+
+async def authorise(reader, writer, token):
+    message = token + "\n"
+    writer.write(message.encode())
+    await writer.drain()
+    data = await reader.readline()
+    data = json.loads(data)
+    if data:
+        logging.debug(f"Пользователь {data['nickname']} авторизован.")
+        return True
+    else:
+        logging.debug("Токен невалидный. Проверьте его или зарегестрируйтесь заново.")
+        return False
+
+
+async def send_msgs(host, port, queue, token):
+    reader, writer = await asyncio.open_connection(host, port)
+    data = await reader.readline()
+    logging.debug(f"{data.decode()}")
+    if token:
+        auth_status = await authorise(reader, writer, token)
     while True:
         msg = await queue.get()
         print(msg) 
+
 
 def load_history(history, queue):
     with open("minechat.history", 'r') as f:
         for line in f.readlines():
             queue.put_nowait(line)
 
+
 async def main():
-    parser = configargparse.ArgParser(default_config_files=['.env_listen'])
+    parser = configargparse.ArgParser(default_config_files=['.env'])
     parser.add('--host', required=True,
                help='chat host address')
-    parser.add('--port', required=True, type=int,
+    parser.add('--port_listen', required=True, type=int,
                help='chat listen port')
     parser.add('--history', required=True,
                help='history writing file')
+    parser.add('--port_write', required=True, type=int,
+               help='chat write port')
+    parser.add('--token', help='user connection token')   
     args = parser.parse_args()
-    host, port, history = args.host, args.port, args.history
+    host, port_listen, history = args.host, args.port_listen, args.history
+    port_write, token = args.port_write, args.token
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
@@ -75,8 +103,8 @@ async def main():
     load_history(history, messages_queue)
     await asyncio.gather(
             save_messages(history, history_queue),
-            send_msgs(host, port, sending_queue),
-            read_msgs(host, port, messages_queue, history_queue),
+            send_msgs(host, port_write, sending_queue, token),
+            read_msgs(host, port_listen, messages_queue, history_queue),
             gui.draw(messages_queue, sending_queue, status_updates_queue)
             )
 
