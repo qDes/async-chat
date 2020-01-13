7b73657756a3dc6b1c5e2751da3fedc692c13057
Andrei K <1____1@bk.ru>
2020-01-14 00:12:23 +0300

    get rid off gather from gui.py

--- gui.py
+++ gui.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 import asyncio
+import contextlib
+
+from aionursery import Nursery, MultiError
 from tkinter.scrolledtext import ScrolledText
 from enum import Enum
+from tools import create_handy_nursery
 
 
 class TkAppClosed(Exception):
@@ -125,9 +129,10 @@ async def draw(messages_queue, sending_queue, status_updates_queue):
 
     conversation_panel = ScrolledText(root_frame, wrap='none')
     conversation_panel.pack(side="top", fill="both", expand=True)
-
-    await asyncio.gather(
-        update_tk(root_frame),
-        update_conversation_history(conversation_panel, messages_queue),
-        update_status_panel(status_labels, status_updates_queue)
-    )
+    
+    async with create_handy_nursery() as nursery:
+        nursery.start_soon(update_tk(root_frame))
+        nursery.start_soon(update_conversation_history(conversation_panel,
+                                                       messages_queue))
+        nursery.start_soon(update_status_panel(status_labels,
+                                               status_updates_queue))
